78a7204afa9e90be64da2c1cf0f223079596f082
jonathan_fambro <startek591@gmail.com>
2026-07-01 09:42:28 -0400

    feat:add testing suite

---
+++ 2-databinding-first-app/1-hello-world/hello.spec.js
@@ -0,0 +1,29 @@
+describe("HelloUser component", function () {
+  let $compile;
+  let $rootScope;
+
+  beforeEach(module("helloApp"));
+
+  beforeEach(inject(function (_$compile_, _$rootScope_) {
+    $compile = _$compile_;
+    $rootScope = _$rootScope_;
+  }));
+
+  it("should display the bound name", function () {
+    const scope = $rootScope.$new();
+
+    scope.name = "Jonathan";
+
+    const element = $compile(
+      `<input ng-model="name" />
+      <h1>Hello {{ name }}</h1>
+      `,
+    )(scope);
+
+    scope.$digest();
+
+    scope.$digest();
+
+    expect(element.text()).toContain("Hello Jonathan");
+  });
+});
