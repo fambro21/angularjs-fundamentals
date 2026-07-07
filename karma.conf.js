39df98aea45b96d77703a1dd5d1d04e146cee313
jonathan_fambro <startek591@gmail.com>
2026-07-06 20:20:10 -0400

    chore:add basic directive to testing config

--- karma.conf.js
+++ karma.conf.js
@@ -34,6 +34,9 @@ module.exports = function (config) {
 
       "5-filters/2-making-a-custom-filter/*.js",
       "5-filters/2-making-a-custom-filter/*.spec.js",
+
+      "6-beginning-directives/first-directive/*.js",
+      "6-beginning-directives/first-directive/*.spec.js",
     ],
 
     // list of files / patterns to exclude
